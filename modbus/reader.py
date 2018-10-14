38e783020f1cf9efecdce8196fc530e89e067e82
Dmitri Zimine <dz@stackstorm.com>
2018-10-14 07:30:48 -0700

    Write, serve, and read pseudo-motor values.
    Reading only int for now.
    TODO next - read different types.

--- modbus/reader.py
+++ modbus/reader.py
@@ -4,41 +4,55 @@
 
 import logging
 import sys
+import json
 
-# import pymodbus libraries for the modbus client
 from pymodbus.client.sync import ModbusTcpClient as ModbusClient
-from pymodbus.payload import BinaryPayloadDecoder
-# from pymodbus.payload import BinaryPayloadBuilder
-from pymodbus.constants import Endian
 
-# Instantiate the client for your modbus slave device. In this example we are
-# using the local IP address where a simulator exists.
-mbclient = ModbusClient('127.0.0.1', port=5020)
+HOST = '127.0.0.1'
+PORT = 5020
 
-# Default port for modbus slave is typically 502. Using 5020 for simulation to
-# avoid root permissions.
 logger = logging.getLogger(__name__)
 logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 
+mbclient = ModbusClient('127.0.0.1', port=5020)
+
 
 # Get the values from a modbus slave device (simulator)
+# TODO: Reading different data types (e.g. 32bit_float)
 def get_value():
+
+    # Syntaxic sugar to get registers off the blocks
+    #
+    addr_from = 0
+
+    def get(register):
+        return rr.registers[register - addr_from]
+
     try:
-        # connect to modbus slave device
-        mbclient.connect()
-        # set the address and number of bytes that will be read on the modbus device
-        address = 0x00
-        count = 8
-        # read the holding register value for the temperature
-        rr = mbclient.read_holding_registers(address, count, unit=1)
-        # decode results as a 32 bit float
-        decoder = BinaryPayloadDecoder.fromRegisters(rr.registers, wordorder=Endian.Big)
-        decoded = {
-            'humdity': decoder.decode_32bit_float(),
-            'light': decoder.decode_8bit_int(),
-            'temp': decoder.decode_32bit_float()
+        # Read a continious block of registers [from...to], parse out the values
+        addr_from = 1202
+        count = 10
+        rr = mbclient.read_holding_registers(addr_from, count, unit=1)
+        d = {
+            'frequency': get(1202),
+            'current': get(1204),
+            'torque': get(1205),
+            'voltage': get(1208),
+            'power': get(1211)
         }
-        logging.info("Value decoded: {0}".format(decoded))
+
+        # Read a continious block of registers [from...to], parse out the values
+        addr_from = 2004
+        count = 9
+        rr = mbclient.read_holding_registers(addr_from, count, unit=1)
+        d['speed_SPD'] = get(2004)
+        d['speed_SPDM'] = get(2011)
+        d['speed_SPD1'] = get(2012)
+
+        # Compute the power
+        d['power_computed'] = d['torque'] * (d['speed_SPD'] / 5252.0)
+
+        logging.info("Value decoded: {0}".format(json.dumps(d, indent=4)))
     except Exception, e:
         logging.info("Error: {0}".format(str(e)))
 
--- modbus/simulator.py
+++ modbus/simulator.py
@@ -5,16 +5,14 @@
 import time
 import logging
 import sys
-import random
 
 from pymodbus.client.sync import ModbusTcpClient as ModbusClient
 from pymodbus.payload import BinaryPayloadBuilder
 from pymodbus.constants import Endian
-# from pymodbus.compat import iteritems
 
-# Default port for modbus slave is typically 502. Using 5020 for simulation to
-# avoid root permissions.
-client = ModbusClient('127.0.0.1', port=5020)
+HOST = '127.0.0.1'
+PORT = 5020
+UNIT = 1
 
 # instantiate logger which will log any exceptions to Cloudwatch or Greengrass
 # local logs
@@ -28,65 +26,111 @@ def get_decoder(typename, bits):
 
 
 def get_builder(typename, bits):
+    # returns a function pointing to a method of a typename
+    # like add_16bit_int
+
     name = "add_{0}bit_{1}".format(bits, typename)
     return lambda d: getattr(d, name)
 
 
-def build_payload():
-    '''
-    Simulate the following payload:
-      1. humdity - float32 (eg 45.3 %)
-      2. light - uint (0 or 1)
-      3. temp float32 (eg 21.3 C)
+registers = [
+    {
+        'displayName': 'frequency',
+        'address': 1202,
+        'dtype': 'float',
+        'bits': 32,
+        'value': 1202.222
+    },
+    {
+        'displayName': 'current',
+        'address': 1204,
+        'value': 1204.444
+    },
+    {
+        'displayName': 'torque',
+        'address': 1205,
+        'value': 1205.555
+    },
+    {
+        'displayName': 'voltage',
+        'address': 1208,
+        'value': 1208.888
+    },
+    {
+        'displayName': 'power',
+        'address': 1211,
+        'value': 1211.111
+    },
+    {
+        'displayName': 'speed_SPD',
+        'address': 2004,
+        'value': 2004.444
+    },
+    {
+        'displayName': 'speed_SPDM',
+        'address': 2011,
+        'value': 2011.111
+    },
+    {
+        'displayName': 'speed_SPD1',
+        'address': 2012,
+        'value': 1202.222
+    },
+    {
+        'displayName': 'device_id',
+        'address': 1012,
+        'dtype': 'string',
+        'value': 'dev0'
+    },
+]
 
-    '''
-    state = [
-        {
-            'displayName': 'humidity',
-            'type': 'float',
-            'bits': 32,
-            'value': float(random.random())
-        },
-        {
-            'displayName': 'light',
-            'type': 'uint',
-            'bits': 8,
-            'value': int(random.choice([0, 1]))
-        },
-        {
-            'displayName': 'temp',
-            'type': 'float',
-            'bits': 32,
-            'value': float(random.random())
-        }
 
-    ]
+class RegisterWriter():
 
-    builder = BinaryPayloadBuilder(wordorder=Endian.Big)
+    def __init__(self, host, port):
+        self.mb_client = ModbusClient(host, port=port)
+        self.mb_client.connect()
 
-    values = []
-    for s in state:
-        values.append("{0} = {1};".format(s['displayName'], s['value']))
-        get_builder(s['type'], s['bits'])(builder)(s['value'])
+    def write(self, displayName, address, value, dtype=None, bits=None):
+        if dtype:
+            builder = BinaryPayloadBuilder(wordorder=Endian.Big)
 
-    logging.info("Writing values: " + "".join(values))
+            # This code trick generates and calls methods of PayloadBuilder,
+            # like `builder.add_16bit_int(r['value'])
+            # where 16 = r[bits], int = r['typename']
+            if dtype == 'string':
+                method_name = "add_string"
+            else:
+                method_name = "add_{0}bit_{1}".format(bits, dtype)
+            getattr(builder, method_name)(value)
 
-    return builder.build()
+            payload = builder.build()
+
+            logging.debug("Packed value '{0}' with method {1} into {2}".format(
+                value, method_name, payload))
+
+            self.mb_client.write_registers(address, payload, skip_encode=True, unit=UNIT)
+
+        else:
+            logging.debug("Wrote value '{0}' to 16bit register".format(value))
+            self.mb_client.write_register(address, value, unit=UNIT)
 
 
 # in an infinite loop, this procedure will poll the cpu temperature and write
 # it to a local modbus slave device.
 def poll_temp():
+    writer = RegisterWriter(HOST, PORT)
+
     while True:
         try:
-            client.connect()
-            address = 0
-            payload = build_payload()
-            client.write_registers(address, payload, skip_encode=True, unit=1)
-            logging.info("Wrote to holding registers: {0}".format(str(payload)))
+            for r in registers:
+                writer.write(**r)
+
+            logging.info("Wrote to holding registers: {0}".format(str(registers)))
 
         except Exception as e:
             logging.info("Error: {0}".format(str(e)))
+            raise(e)
 
         time.sleep(5)
 
