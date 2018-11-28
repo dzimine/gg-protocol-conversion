22ccba90e7b884578ca976fd537df6904dfab141
Dmitri Zimine <dz@stackstorm.com>
2018-11-28 15:33:04 -0800

    Upgrade to Greengrass v1.7

--- scripts/install.sh
+++ scripts/install.sh
@@ -8,7 +8,7 @@ sudo apt-get install -y sqlite3 python2.7 binutils curl
 wget -O /vagrant/downloads/root.ca.pem http://www.symantec.com/content/en/us/enterprise/verisign/roots/VeriSign-Class%203-Public-Primary-Certification-Authority-G5.pem
 
 # Copy greengrass binaries
-sudo tar -xzf /vagrant/downloads/greengrass-ubuntu-x86_64-1.6.99.tar.gz -C /
+sudo tar -xzf /vagrant/downloads/greengrass-linux-x86-64-1.7.0.tar.gz -C /
 
 # # Back up group.json - you'll thank me later
 # sudo cp /greengrass/ggc/deployment/group/group.json /greengrass/ggc/deployment/group/group.json.orig
@@ -18,16 +18,14 @@ sudo apt-get install -y python-dev python-setuptools gcc
 sudo easy_install pip
 
 # Install python dependencies for Lambda functions
-# It includes the hack to put AWS Greengrass Core SDK from ./downloads in site-packages,
-# because the damn thing doesn't install as a normal python dependency.
 cd /vagrant
-./scripts/build_py.sh
+./scripts/build-py.sh
 
-# Install Modbus requirements on the host, for Modbus slave and simulator
-sudo pip install -r /vagrant/requirements.txt
+# Hack to put AWS Greengrass Core SDK in the right place,
+# because the damn thing doesn't install as a normal python dependency.
+    # mkdir -p /vagrant/lambdas/ModbusToAWSIoT/site-packages
+tar -xzf /vagrant/downloads/greengrass-core-skd-1.2.0-dz.tar.gz -C /vagrant/lambdas/ModbusToAWSIoT/site-packages/
 
-# Post install steps:
-# 1. run greengo on the host system to provision group in AWS
-# 2. copy certificates - ./scripts/update_ggc.sh
-# 3. sudo /greengrass/ggc/core/greengrassd start
 
+# cd /greengrass/ggc/core
+# sudo ./greengrassd start
